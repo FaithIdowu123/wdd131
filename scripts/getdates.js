1a7115ddd1f7e1ed24658abfd35fd29dcd37dc6d
FaithIdowu123 <ofaith@byupathway.edu>
2025-07-03 23:37:19 +0100

    change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ document.getElementById("currentyear").textContent = `@ ${string}`;
 
 document.getElementById("lastModified").textContent = "Last modification: " + document.lastModified;
 
-document.getElementById("course-title").textContent = "WDD 131: Dynamic Web Fundamentals - "
+document.getElementById("course-title").textContent = "WDD 131: Dynamic Web Fundamentals"
